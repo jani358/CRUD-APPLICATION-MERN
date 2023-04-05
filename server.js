45a597d9971972ec24ff08d1ac13e9de70ae4937
Janindu Gayanga <83686809+jani358@users.noreply.github.com>
2023-04-05 15:51:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-//the information is sent to the server in json format. the server cannot understand the incoming format, the server can understand the javascript object, and the incoming request needs to be converted into the javascript object. that is what body-parser is used for.
+//the information is sent to the server in json format. the server cannot understand the incoming format, 
+//the server can understand the javascript object, and the incoming request needs to be converted into the javascript object. that is what body-parser is used for.
 const bodyParser = require('body-parser');
 
 //when running from backend and frontend port number two,it is blocked for a security reason
