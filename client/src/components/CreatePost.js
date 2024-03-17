ecdfa30e40c4dd6b224886868f935015f914b209
Janindu Gayanga <83686809+jani358@users.noreply.github.com>
2024-03-17 23:52:14 +0530

    Update CreatePost.js

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class CreatePost extends Component {
   constructor(props){
     super(props);
+    // State to hold the form input values
     this.state={
       topic:"",
       description:"",
@@ -11,27 +12,30 @@ export default class CreatePost extends Component {
     }
   }
 
+  // Function to handle input changes in the form
   handleInputChange = (e) =>{
-    const {name,value} = e.target;
+    const {name, value} = e.target;
     this.setState({
       ...this.state,
-      [name]:value
+      [name]: value
     })
   }
 
-
+  // Function to handle form submission
   onSubmit = (e) =>{
     e.preventDefault();
-    const {topic,description,postCategory} = this.state;
+    const {topic, description, postCategory} = this.state;
     const data ={
-      topic:topic,
-      description:description,
-      postCategory:postCategory
+      topic: topic,
+      description: description,
+      postCategory: postCategory
     }
-    console.log(data)
+    console.log(data);
 
-    axios.post("/post/save",data).then((res) =>{
+    // Sending a POST request to save the post data
+    axios.post("/post/save", data).then((res) =>{
       if(res.data.success){
+        // If post is successfully saved, clear the form inputs
         this.setState(
           {
             topic:"",
@@ -45,50 +49,61 @@ export default class CreatePost extends Component {
 
   render() {
     return (
-        <div className="col-md-8 mt-4 mx-auto">
-          <h1 className="h3 mb-3 font-weight-normal">Create new post</h1>
-          <form className="needs-validation" noValidate>
-            <div className="form-group" style={{marginBottom:'15px'}}>
-              <label style={{marginBottom:'5px'}}>Topic</label>
-              <input type="text"
+      <div className="col-md-8 mt-4 mx-auto">
+        <h1 className="h3 mb-3 font-weight-normal">Create new post</h1>
+        <form className="needs-validation" noValidate>
+          {/* Input for Topic */}
+          <div className="form-group" style={{marginBottom:'15px'}}>
+            <label style={{marginBottom:'5px'}}>Topic</label>
+            <input 
+              type="text"
               className="form-control"
               name="topic"
               placeholder="Enter Topic"
               value={this.state.topic}
-              onChange={this.handleInputChange}/>
-            </div>
+              onChange={this.handleInputChange}
+            />
+          </div>
 
-            <div className="form-group" style={{marginBottom:'15px'}}>
-              <label style={{marginBottom:'5px'}}>Description</label>
-              <input type="text"
+          {/* Input for Description */}
+          <div className="form-group" style={{marginBottom:'15px'}}>
+            <label style={{marginBottom:'5px'}}>Description</label>
+            <input 
+              type="text"
               className="form-control"
               name="description"
               placeholder="Enter Description"
               value={this.state.description}
-              onChange={this.handleInputChange}/>
-            </div>
+              onChange={this.handleInputChange}
+            />
+          </div>
 
-
-            <div className="form-group" style={{marginBottom:'15px'}}>
-              <label style={{marginBottom:'5px'}}>Post Category</label>
-              <input type="text"
+          {/* Input for Post Category */}
+          <div className="form-group" style={{marginBottom:'15px'}}>
+            <label style={{marginBottom:'5px'}}>Post Category</label>
+            <input 
+              type="text"
               className="form-control"
               name="postCategory"
               placeholder="Enter Post Category"
               value={this.state.postCategory}
-              onChange={this.handleInputChange}/>
-            </div>
+              onChange={this.handleInputChange}
+            />
+          </div>
              
-            <button className="btn btn-success" type="submit" style={{marginTop:'15px'}} onClick={this.onSubmit}>
-              <i className="far fa-check-square"></i>
-              &nbsp; Save
-            </button>
+          {/* Button to Submit the Form */}
+          <button 
+            className="btn btn-success" 
+            type="submit" 
+            style={{marginTop:'15px'}} 
+            onClick={this.onSubmit}
+          >
+            <i className="far fa-check-square"></i>
+            &nbsp; Save
+          </button>
 
-          </form>
-        </div>
-
-            
-
-    )
+        </form>
+      </div>
+    );
   }
 }
